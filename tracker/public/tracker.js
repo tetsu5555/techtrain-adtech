eabc953ff41482acd19daaae6b239be89db6f8d2
tetsu5555 <tetete1118@gmail.com>
2020-04-08 11:04:36 +0900

    feat: add tracker

--- tracker/public/tracker.js
+++ tracker/public/tracker.js
@@ -1,3 +1,21 @@
 window.onload = function() {
   alert("hello world");
+
+  // credentials: 'same-origin'を追加。リクエスト URL が呼び出しスクリプトと同一オリジンの場合だけクレデンシャルを送りたい場合
+  // credentials: 'include'を設定することで別のオリジンからのCookieがセットされるようになった
+  fetch('http://localhost:8080', 
+    {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Cache': 'no-cache'
+      },
+      credentials: 'include'
+    }
+  )
+    .then(function(response) {
+      console.log(response.headers['set-cookie']);
+      console.log(response.headers.get('set-cookie'));
+      console.log(response)
+    })
 }
