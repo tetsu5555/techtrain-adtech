a80e48b8206cdf9e2ec444905cd70eb39877bc07
tetsu5555 <tetete1118@gmail.com>
2020-04-09 00:33:23 +0900

    feat: update tracker.js

--- tracker/public/tracker.js
+++ tracker/public/tracker.js
@@ -1,23 +1,11 @@
 window.onload = async function() {
-  alert("trackerがロードされました");
+  // alert("trackerがロードされました");
 
   // credentials: 'same-origin'を追加。リクエスト URL が呼び出しスクリプトと同一オリジンの場合だけクレデンシャルを送りたい場合
   // credentials: 'include'を設定することで別のオリジンからのCookieがセットされるようになった
-  await fetch('http://localhost:8080', 
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Cache': 'no-cache'
-      },
-      credentials: 'include'
-    }
-  )
-  .then(function(response) {
-    console.log(response)
-  })
-
-  await fetch('http://localhost:8080/count', 
+  const site_url = 'http://localhost:8080'
+  const query = `?path=${window.location.pathname}`
+  await fetch(site_url + query,
     {
       headers: {
         'Accept': 'application/json',
@@ -35,13 +23,18 @@ window.onload = async function() {
   })
   .then((data) => {
     console.log(data)
-    if (data.count >= 5)  {
-      const div = document.createElement("div")
-      div.innerText = "5回以上の訪問ありがとうございます！！"
-      document.body.appendChild(div)
-    }
+    setTimeout(() => {
+      const el = document.createElement( 'html' );
+      el.innerHTML = data.text
+      document.body.appendChild(el)
+    }, 1000)
   })
   .catch((reason) => {
     console.log(reason);
   });
+}
+
+// TODO: trackerっぽいやつを定義する
+function hello_world() {
+  console.log("hello world")
 }
