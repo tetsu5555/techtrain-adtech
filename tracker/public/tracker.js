2058fa9b80c5af5fbc0952d69072c6537a28c54b
tetsu5555 <tetete1118@gmail.com>
2020-04-08 11:56:48 +0900

    feat: update tracker.js

--- tracker/public/tracker.js
+++ tracker/public/tracker.js
@@ -1,9 +1,9 @@
-window.onload = function() {
-  alert("hello world");
+window.onload = async function() {
+  alert("trackerがロードされました");
 
   // credentials: 'same-origin'を追加。リクエスト URL が呼び出しスクリプトと同一オリジンの場合だけクレデンシャルを送りたい場合
   // credentials: 'include'を設定することで別のオリジンからのCookieがセットされるようになった
-  fetch('http://localhost:8080', 
+  await fetch('http://localhost:8080', 
     {
       headers: {
         'Accept': 'application/json',
@@ -13,9 +13,35 @@ window.onload = function() {
       credentials: 'include'
     }
   )
-    .then(function(response) {
-      console.log(response.headers['set-cookie']);
-      console.log(response.headers.get('set-cookie'));
-      console.log(response)
-    })
+  .then(function(response) {
+    console.log(response)
+  })
+
+  await fetch('http://localhost:8080/count', 
+    {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Cache': 'no-cache'
+      },
+      credentials: 'include'
+    }
+  )
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data)
+    if (data.count >= 5)  {
+      const div = document.createElement("div")
+      div.innerText = "5回以上の訪問ありがとうございます！！"
+      document.body.appendChild(div)
+    }
+  })
+  .catch((reason) => {
+    console.log(reason);
+  });
 }
