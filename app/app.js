d85037c71b3c948fc8952cad9fc10b0d081e6cd8
tetsu5555 <tetete1118@gmail.com>
2020-04-08 11:57:07 +0900

    feat: upate app.js

--- app/app.js
+++ app/app.js
@@ -8,13 +8,38 @@ app.use(cors({ origin: true, credentials: true }))
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
+const data = {}
+
 app.get('/', (req, res) => {
 
-  console.log(req.cookies)
-  
-  res.cookie("msg", "hoge");
+  console.log("cookie", req.cookies)
+
+  if (req.cookies.userId) {
+    // 二回以降の訪問
+    const userId = req.cookies.userId
+    if (data[userId]) {
+      data[userId] = data[userId] + 1
+    }
+  } else {
+    // 初回訪問
+    // TODO: 被らないキーを発行する
+    const userId = Date.now()
+    res.cookie("userId", userId);
+    data[userId] = 1
+  }
+
+  console.log("this is data", data)
   
   res.send('Hello World!')
+})
+
+app.get('/count', (req, res) => {
+  if (req.cookies.userId) {
+    res.json({ count: data[req.cookies.userId] })
+  } else {
+    // TODO: cookieがない場合どうするか？
+    res.json({ count: 0 })
+  }
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
